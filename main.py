cae37fc2a7724e4f9632f4644b6a059a9c43081e
Shikhar Aryal <ranktone021@gmail.com>
2026-09-13 11:59:26 +0545

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Enter number: "))
-for i in range(1, n+1):
-    if n % i == 0:
-        dsum = sum(map(int, str(i)))
-        print(i, "is Harshad" if i % dsum == 0 else "is not Harshad")
