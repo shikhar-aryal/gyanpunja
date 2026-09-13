49b513cad61c9f9b97a7d0ceb0c046d552d2b8e9
Shikhar Aryal <ranktone021@gmail.com>
2026-09-13 10:28:35 +0545

    Delete real.js

--- real.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.anim-section');
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top <= window.innerHeight / 1.5) {
-            section.classList.add('show');
-        }
-    });
-});
